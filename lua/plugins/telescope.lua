99a127e804a64411750bcebcc9ba19d5b5287552
Yasser <y.alattas@gmail.com>
2025-12-20 23:59:18 +0300

    chore(telescope): make it vertical

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,19 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require("lsp-file-operations").setup({
+        defaults = {
+          layout_config = {
+            vertical = { width = 0.5 },
+            -- other layout configuration here
+          },
+          -- other defaults configuration here
+        },
+      })
+    end,
   }
 }
