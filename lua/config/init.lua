1e2b72f50d3ca329734086764feea6b1f06ab293
Yasser <y.alattas@gmail.com>
2026-02-06 23:57:58 +0300

    chore(indentation): switch to spaces

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -10,3 +10,10 @@ vim.opt.termguicolors = true
 
 -- Use system clipboard for yank and paste operations
 vim.opt.clipboard = "unnamed,unnamedplus"
+
+-- Indentation settings: use spaces instead of tabs
+vim.opt.expandtab = true      -- Convert tabs to spaces
+vim.opt.shiftwidth = 2        -- Number of spaces for indentation
+vim.opt.tabstop = 2           -- Number of spaces that a tab counts for
+vim.opt.softtabstop = 2       -- Number of spaces for tab in insert mode
+vim.opt.smartindent = true    -- Smart autoindenting on new lines
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,6 +14,12 @@ return {
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
+			formatters = {
+				-- Configure stylua to use spaces
+				stylua = {
+					prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+				},
+			},
 			format_on_save = {
 				-- These options will be passed to conform.format()
 				timeout_ms = 500,
